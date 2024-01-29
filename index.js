79ff7ae8c2118525a3335a57f4cc72c9c8b0d49b
LDdotri <158063460+LDdotri@users.noreply.github.com>
2024-01-29 21:42:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+let tuoi;
+
+document.getElementById("sm").onclick = function(){
+    tuoi = document.getElementById("st").value;
+    document.getElementById("p1").textContent = `Bạn đã ${tuoi} tuổi rồi!`;
+}
+
+
 
